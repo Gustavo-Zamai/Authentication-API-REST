755de4fa09e8325de063d5dfb96db48e8d112b1c
Gustavo Simão Zamai <gustavosimaozamai@gmail.com>
2022-01-25 22:41:57 -0300

    Add methods GET and install http-status-codes, and export usersRoute.

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,18 +1,26 @@
-import { Router, Request, Response, NextFunction } from 'express';
-
+import { Router, Request, Response, NextFunction, response } from 'express';
+import { StatusCodes } from 'http-status-codes';
 const usersRoute = Router();
-//  get/users
 
+//  get/users
 usersRoute.get('/users', (request: Request, response: Response, next: NextFunction) => {
     const users = [{userName: 'Gustavo'}];
-    response.status(200).send({ users });
+    response.status(StatusCodes.OK).send({ users });
 });
-
-export default usersRoute;
 
 
 //  get/users/:uuid
+usersRoute.get('/users/:uuid', (request: Request <{uuid: string}>, response: Response, next: NextFunction) => {
+    const uuid = request.params.uuid;
+    //BD.getUserByUUid(uuid);
+    response.status(StatusCodes.OK).send({ uuid });
+});
+
+
+
+
 //  post/users
 //  put/users/:uuid
 //  delete/users/:uuid
 
+export default usersRoute;
