569efa0f3c33d71a64e4e42b841593d4ba153f47
Gustavo Simão Zamai <gustavosimaozamai@gmail.com>
2022-01-25 22:05:36 -0300

    Add typescript, express and a better way to develop.

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,1 +1,11 @@
-console.log('Running');
+import express, {Request, Response, NextFunction} from "express";
+
+const app = express();
+
+app.get('/status', (request: Request, response: Response, next: NextFunction) => {
+    response.status(200).send({foo: 'bar'});
+});
+
+app.listen(3000, () => {
+    console.log('Running on port 3000');
+});
